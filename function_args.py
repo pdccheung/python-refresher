cd6c39367f7f43127a3317317c880831d56e23a4
philipintech <philipintech@gmail.com>
2022-02-02 15:53:26 -0500

    py functions

---
+++ function_args.py
@@ -0,0 +1,17 @@
+# *args parameters 
+# the type for the args is a tuple (immutable list)
+
+def numbers (name, *args):
+    print(type(args))
+    print(type(name))
+
+numbers("Philip", 1,2,3,4,5,)
+
+
+# **kwargs parameters
+# the type for the key-word args is a dictionary (dict) 
+def kw_args (**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+kw_args(first_key="My first name", next_key="My last name", age=28)
